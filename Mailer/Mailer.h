b4375ac2cde44fc85b077ad8e79f073394d8f8d0
LarsBeentjes <lars.jelger.beentjes@gmail.com>
2017-10-23 15:14:13 +0200

    Create Mailer.h

---
+++ Mailer/Mailer.h
@@ -0,0 +1,5 @@
+/*
+Responsible for mailing a given message to a username; processes username to make e-mail out of it. Use standardized sxxxxxxx.liacs.nl
+(Is it like that?) and a file with custom username-email lines.
+
+*/ 
