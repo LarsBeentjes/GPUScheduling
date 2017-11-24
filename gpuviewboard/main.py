80f5e3f7c6dad841f27a0dfe1ca8dad7ac18ea9f
Rob <rob@reijtenbach.com>
2017-11-24 20:32:34 +0100

    Viewboard geeft nu wat meer info
    Mocht Kean (of iemand anders er nog naar willen kijken dan kan dat)

--- gpuviewboard/main.py
+++ gpuviewboard/main.py
@@ -3,9 +3,18 @@
 import curses
 import time
 import sys
+import json
 
 sys.path.insert(0, '../monitor_client')
 from MonitorClient import MonitorClient
+
+def uptime():                                                                                                        
+    uptime = 0.0
+    with open('/proc/uptime') as fp:
+        line = fp.readline()
+        segments = line.split()
+        uptime = float(segments[0])
+    return uptime
 
 def main(stdscr):
     monitor_client = MonitorClient('/home/s1485873/monitor.socket')
@@ -14,8 +23,10 @@ def main(stdscr):
         stdscr.clear()
         stdscr.addstr('This is the GPU overview board\n\n')
         for proc in monitor_client.get_process_data():
-            stdscr.addstr('user {0}  {0} proc {1}\n'.format(proc['fullname'], proc['process_name']))
-            
+            stdscr.addstr('user: {}\nfull name: {}\nproccess name: {}\nprocess time: {}\n\n'.format(proc['username'], proc['fullname'], 
+            proc['process_name'], int(time.time())-int(proc['proc_birth'])))
+            #De tijd is nog een beetje van slag omdat De Jonckheere daar lugubere dingen mee heeft gedaan
+            #De tijd is getraumatiseerd zou je kunnen zeggen
         stdscr.refresh()
         time.sleep(1.0)
 
@@ -26,3 +37,4 @@ if __name__ == '__main__':
         main(scr)
     finally:
         curses.endwin()
+
