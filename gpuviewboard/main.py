4e0feed0bbc97652e1ba0031e643eb47a3e3f41c
Rob <rob@reijtenbach.com>
2017-11-24 13:13:06 +0100

    Werkend begin viewboard
    Doet nu ook daadwerkelijk iets

--- gpuviewboard/main.py
+++ gpuviewboard/main.py
@@ -1,8 +1,28 @@
 #!/bin/python3
 
+import curses
+import time
+import sys
 
-def main():
-    print('This is the GPU overview board')
+sys.path.insert(0, '../monitor_client')
+from MonitorClient import MonitorClient
+
+def main(stdscr):
+    monitor_client = MonitorClient('/home/s1485873/monitor.socket')
+
+    while True:
+        stdscr.clear()
+        stdscr.addstr('This is the GPU overview board\n\n')
+        for proc in monitor_client.get_process_data():
+            stdscr.addstr('user {0}  {0} proc {1}\n'.format(proc['fullname'], proc['process_name']))
+            
+        stdscr.refresh()
+        time.sleep(1.0)
+
 
 if __name__ == '__main__':
-    main()
+    scr = curses.initscr()
+    try:
+        main(scr)
+    finally:
+        curses.endwin()
