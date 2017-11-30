f2df2dcd2cdd11f952264f953de2e5d361b40af0
LarsBeentjes <lars.jelger.beentjes@gmail.com>
2017-11-30 19:12:38 +0100

    Fixed fucked spaces

--- Mailer/Mail_Controller.py
+++ Mailer/Mail_Controller.py
@@ -8,11 +8,11 @@ from collections import Counter
 from MonitorClient import MonitorClient
 
 OwnPath = sys.path[0] + '/'			#Note change this to be compatible with where the program is actually invoked.
-									#Is supposed to point to Mailer directory.
-									#sys.path[0] points to where the program is initially called.
-									#So if monitor is where the program is ultimately started.
-									#Then it points to the monitor directory.
-									#And we need to add /../Mailer/
+						#Is supposed to point to Mailer directory.
+						#sys.path[0] points to where the program is initially called.
+						#So if monitor is where the program is ultimately started.
+						#Then it points to the monitor directory.
+						#And we need to add /../Mailer/
 
 def exception(user):
     try:
@@ -29,7 +29,7 @@ def exception(user):
     return False
 
 def main():
-    monitor_client = MonitorClient('/home/s1485873/monitor.socket')			#Probably also needs to be changed, but not sure how.
+    monitor_client = MonitorClient('/home/s1485873/monitor.socket')	#Probably also needs to be changed, but not sure how.
     mailer = Mailer()
     host = socket.gethostname().split('.',1)[0]
     
@@ -38,41 +38,40 @@ def main():
         violations = {}
         pids = set()
         data = monitor_client.get_process_data()
-        counted = Counter(data['pid'])							#I'm reasonably sure the monitor client returns a dictionary and that this should work.
-																#But not entirely certain. Can someone check this?
-																#Here for efficiency.
-																
+        counted = Counter(data['pid'])			#I'm reasonably sure the monitor client returns a dictionary and that this should work.
+							#But not entirely certain. Can someone check this?
+							#Here for efficiency.					
         try:
             with open(OwnPath + 'Rules', 'r') as Rulesfile:
                 for line in Rulesfile:
-					bool ruleapplies = False
-					bool isitall = False
-					helptext = line.split()
-					helptext2 = helptext
+                    bool ruleapplies = False
+                    bool isitall = False
+                    helptext = line.split()
+                    helptext2 = helptext
                     if(len(helptext) < 6):	#Thusly, might be a valid rule.
                         continue
                     if(isinstance(helptext[1], int) and isinstance(helptext[2], int) and isinstance(helptext[3], int) and isinstance(helptext[5], int)):
-						if(helptext[0] == 'ALL'):
-							isitall = True
-						else:
-							with open(OwnPath + 'Groups', 'r') as Groupsfile:
-								for grouplines in Groupsfile:
-									grouphelp = grouplines.split()
-									if(grouphelp[0] == helptext[0]): #The group name matches
-										ruleapplies = True
-										helptext2 = grouphelp
-										break
-									else:
-										continue
+                        if(helptext[0] == 'ALL'):
+                            isitall = True
+                        else:
+                            with open(OwnPath + 'Groups', 'r') as Groupsfile:
+                                for grouplines in Groupsfile:
+                                    grouphelp = grouplines.split()
+                                    if(grouphelp[0] == helptext[0]): #The group name matches
+                                        ruleapplies = True
+                                        helptext2 = grouphelp
+                                        break
+                                    else:
+                                        continue
                     else:
-						continue #Also checking if a valid rule.
-					if(ruleapplies or isitall):
-						for proc in data:
-							if((proc['username'] in helptext2) or isitall): #Checks if the username is in the group the rule applies to.
-								sec_running = time.time() - float(proc['proc_birth'])
-								if((counted[proc['pid']] >= helptext[2]) and (sec_running > helptext[1])):
-										violations[proc['pid']] = (helptext[5], proc['username'], proc['fullname'], host, helptext[4])
-										#TODO: Do something here to save interval for this proc pid.
+                        continue #Also checking if a valid rule.
+                    if(ruleapplies or isitall):
+                        for proc in data:
+                            if((proc['username'] in helptext2) or isitall): #Checks if the username is in the group the rule applies to.
+                                sec_running = time.time() - float(proc['proc_birth'])
+                                if((counted[proc['pid']] >= helptext[2]) and (sec_running > helptext[1])):
+                                    violations[proc['pid']] = (helptext[5], proc['username'], proc['fullname'], host, helptext[4])
+                                    #TODO: Do something here to save interval for this proc pid.
         except IOError as e:
             print(e)
             print('Rules file or Groups file does not exist. Creating empty Rules/Groups file.') #Also split this up?
