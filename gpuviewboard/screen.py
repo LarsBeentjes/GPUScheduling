f996e07bfba618cdb19a2f92e57183ff0ee360eb
Kean42 <33091393+Kean42@users.noreply.github.com>
2017-12-20 12:07:46 +0100

    Add files via upload

--- gpuviewboard/screen.py
+++ gpuviewboard/screen.py
@@ -21,7 +21,7 @@ def textmid(s,width):
 	return int((width/2)-(len(s)/2))
 
 def main (self):
-	monitor_client = MonitorClient('/home/s1485873'/monitor.socket)
+	monitor_client = MonitorClient('/home/s1485873/monitor.socket')
 
 	curses.init_pair(1,curses.COLOR_YELLOW,curses.COLOR_BLACK)
 	curses.init_pair(2,curses.COLOR_GREEN,curses.COLOR_BLACK)
