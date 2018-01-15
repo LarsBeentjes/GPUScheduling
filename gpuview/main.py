6adc9f526eaed99e40825674c52afbf80d2d77cc
E.R. van der Zande <elgarvdzande@gmail.com>
2018-01-15 17:18:20 +0100

    Runtime is nu ook zichtbaar in gpuview

--- gpuview/main.py
+++ gpuview/main.py
@@ -40,15 +40,27 @@ def is_gpu_available(gpu_id, processes):
     return True
 
 
+def hms_time(time_seconds):
+    t = round(time_seconds)
+    seconds = t % 60
+    t //= 60
+    minutes = t % 60
+    t //= 60
+    hours = t
+
+    return '{}:{:0>2}:{:0>2}'.format(hours, minutes, seconds)
+
+
+
 def print_gpu(state, gpu, name, utilization, ram):
     print('{:<10} {:<4} {:<20} {:<13} {}'.format(state, gpu, name, utilization, ram))
 
 
-def print_user(fullname, username, minor_number, utilization, pid, proc_name):
-    fullname_width = 20
-    proc_name_width = 41 - fullname_width
-    print('{:<{}} {:<10} {:<4} {:<13} {:<7} {}'.format(fullname[:fullname_width],
-        fullname_width, username, minor_number, utilization, pid, proc_name[:proc_name_width]))
+def print_user(fullname, username, minor_number, utilization, pid, runtime, proc_name):
+    fullname_width = 15
+    proc_name_width = 31 - fullname_width
+    print('{:<{}} {:<10} {:<4} {:<13} {:<7} {:<9} {}'.format(fullname[:fullname_width],
+        fullname_width, username, minor_number, utilization, pid, runtime, proc_name[:proc_name_width]))
 
 
 def main():
@@ -76,11 +88,12 @@ def main():
                 gpu['gpu_utilization'], gpu['mem_utilization'])
 
     print('{:*^80}'.format('PROCESSES'))
-    print_user('Fullname', 'username', 'GPU', 'Utilization', 'PID', 'command')
+    print_user('Fullname', 'username', 'GPU', 'Utilization', 'PID', 'Runtime', 'command')
     for proc in processes:
         gpu = find_gpu(gpus, proc['gpu_id'])
+        runtime = hms_time(time.time() - float(proc['proc_birth']))
         print_user(proc['fullname'], proc['username'], gpu['minor_number'],
-                gpu['gpu_utilization'], proc['pid'], proc['process_name'])
+                gpu['gpu_utilization'], proc['pid'], runtime, proc['process_name'])
     print('{:*^80}'.format(''))
 
 
