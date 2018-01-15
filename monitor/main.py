532835b54d9bfe067ec17cdf0757904a7c83d743
E.R. van der Zande <elgarvdzande@gmail.com>
2018-01-15 12:50:27 +0100

    Monitor can only be started once

--- monitor/main.py
+++ monitor/main.py
@@ -10,35 +10,42 @@ from GPUMonitor import GPUMonitor
 from ConnectionHandler import ConnectionHandler
 
 
+SOCKET_ADDR = '/tmp/monitor.socket'
+
+
 def main():
     logging.basicConfig(level=logging.DEBUG)
     logging.info('GPU monitor is starting up')
 
-    gpu_monitor = GPUMonitor()
-
-    sock_addr = '/tmp/monitor.socket'
     try:
-        os.unlink(sock_addr)
-    except:
-        pass
-    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    sock.bind(sock_addr)
-    os.chmod(sock_addr, stat.S_IROTH | stat.S_IWOTH | stat.S_IXOTH | stat.S_IRGRP | stat.S_IWGRP | stat.S_IXGRP | stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
-    sock.listen(10)
+        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        sock.bind(SOCKET_ADDR)
+        os.chmod(SOCKET_ADDR, stat.S_IROTH | stat.S_IWOTH | stat.S_IRGRP |
+                stat.S_IWGRP | stat.S_IRUSR | stat.S_IWUSR)
+        sock.listen(10)
+    except Exception as e:
+        logging.error('Failed to setup socket: {}'.format(str(e)))
+        logging.info('Try removing "{}" if no other instance of the ' +
+                'monitor is running'.format(SOCKET_ADDR))
+        return
+
+    gpu_monitor = GPUMonitor()
 
     logging.info('Waiting for connections')
     try:
         while True:
             client_conn, client_addr = sock.accept()
-            client_worker = ConnectionHandler(gpu_monitor, client_conn, client_addr)
+            client_worker = ConnectionHandler(gpu_monitor,
+                    client_conn, client_addr)
     except KeyboardInterrupt:
         logging.info('shutting down monitor')
 
+    os.remove(SOCKET_ADDR)
     sock.close()
     gpu_monitor.close()
 
-    #  there are only connetion threads left, wait for them to close before closing the
-    #  main thread
+    #  there are only connetion threads left, wait for them to close before
+    # closing the main thread
     logging.info('Waiting for active connections to close')
     for thread in threading.enumerate():
         if thread.isDaemon():
